a5bc49d944c5531aab5cdb5fea88c3c27cc85a15
Sayed Ali Kumail <enggkumail@gmail.com>
2022-01-07 11:02:38 +0400

    frontend 1

--- deploy.js
+++ deploy.js
@@ -1,17 +1,25 @@
-const HDwalletProvider = require ('truffle-hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const {interfaces , bytecode } = require('./compile.js');
+const {interface,bytecode} = require('./compile.js');
 
-const provider = new HDwalletProvider (
+const provider = new HDWalletProvider(
     'resist orbit swap wash hazard burst cat differ apart turn nerve bench',
     'https://rinkeby.infura.io/v3/471e487734fb41b08fc3af164e2f6f9f'
 );
 
-const web3 = new Web3 (provider);
+const web3 = new Web3(provider);
 
-const deploy = async() => {
-    const accounts = await web3.eth.getAccounts();
-    console.log(accounts);
+const deploy = async () =>{
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+  console.log('Contract is deployed by the manager with address ', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({data : '0x' + bytecode})
+  .send({gas : '2000000', from : accounts[0]})
+
+  console.log('Contract deployed to address', result.options.address);
+  
 }
 
 deploy();
--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,17 @@ import React ,{Component} from 'react';
 class Lottery extends Component {
 render(){
     return (
-        <h1>Hello</h1>
+      <div>
+          <h1>Total Lottery Pool  is 1000</h1>
+          <form>
+              <input type="text" value="1.5" /> 
+              <button type="submit">Participate</button>
+          </form>
+          <hr></hr>
+          <p>The Manager of the lottery decentralized pool app is </p>
+          <button>Pick Winners</button>
+      </div>
+      
     )
 }
 }
